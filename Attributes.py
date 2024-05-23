41494ce0cd403428940e5ff5366c00c9411ecf6f
jimbojay <dekker.i@gmail.com>
2024-05-23 03:16:03 +0200

    worked on attributes

--- Attributes.py
+++ Attributes.py
@@ -106,6 +106,17 @@ print(delete_attributes_df)
 ## Additions
 ####################
 
+# Get relevant rows from attribute overview
+gs1_addition_attributes = gs1_df_attributes[gs1_df_attributes['FieldID'].isin(attribute_add_set)]
+
+# Add data type
+gs1_addition_attributes['INPUT_Data_type'] = gs1_addition_attributes.apply(
+    lambda row: 'DateTime' if row['Format'] == 'DateTime'
+                else 'String' if row['Format'] in ['Text', 'Picklist (T/F)', 'Picklist', 'Boolean']  # Added 'Boolean' here
+                else 'Integer' if (row['Format'] == 'Number' or row['Format'] == 'NumberPicklist') and row['Deci-\nmals'] == '0'
+                else 'Decimal' if row['Format'] == 'Number' or row['Format'] == 'NumberPicklist'
+                else 'Unknown', axis=1)
+
 # Create an empty DataFrame with the same columns as maxeda_df
 additions_attributes_df = pd.DataFrame(columns=maxeda_df.columns)
 
@@ -115,24 +126,109 @@ new_rows = []
 # Iterate over the set to fill the DataFrame
 for field_id in attribute_add_set:
     # Find the corresponding attribute name in gs1_df_attributes
-    attribute_name = gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Attributename English'].iloc[0] if not gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Attributename English'].empty else ''
+    attribute_name_long = gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Attributename English'].iloc[0] if not gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Attributename English'].empty else ''
     
+    if attribute_name_long.endswith(')'):
+        # Find the last occurrence of "(" and extract the string up to that point (excluding the last "(" and everything after)
+        attribute_name = attribute_name_long[:attribute_name_long.rfind('(')].strip()
+    else:
+        attribute_name = attribute_name_long.strip()
+
+    description = gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Definition English'].iloc[0] if not gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Definition English'].empty else ''
+    format = gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Format'].iloc[0] if not gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Format'].empty else ''
+    decimals = gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Deci-\nmals'].iloc[0] if not gs1_df_attributes.loc[gs1_df_attributes['FieldID'] == field_id, 'Deci-\nmals'].empty else ''
+
+    # Determine data_type based on 'Format' and 'Decimals'
+    if format == "Number":
+        data_type = "Integer" if decimals == 0 else "Decimal"
+        display_type = "NumericTextBox"
+    elif format == "DateTime":
+        data_type = "DateTime"
+        display_type = "DateTime"
+    elif format == "Text":
+        data_type = "String"
+        display_type = "t.b.d" # to discuss when text box, when text ares
+    elif format == "Picklist (T/F)":
+        data_type = "String"
+        display_type = "LookupTable"
+    elif format == "Picklist":
+        data_type = "String"
+        display_type = "LookupTable"
+    elif format == "NumberPicklist":
+        data_type = "Integer" if decimals == 0 else "Decimal"
+        display_type = "NumericTextBox"
+    elif format == "Boolean":
+        data_type = "String"
+        display_type = "LookupTable"
+    else:
+        data_type = "Unknown"
+        display_type = "Unknown"
+
+    if data_type == "Decimal":
+        is_inheritable = "NO" # to discuss
+        is_lookup = "NO"
+        show_at_entity_creation = "YES" # to discuss
+        precision = decimals
+        use_arbitrary_precision = "NO"
+        allowable_values= ''
+        lookup_table_name= ''
+        lookup_display_columns= ''	
+        lookup_search_columns= ''
+        lookup_display_format= ''
+        lookup_sort_order= ''
+        export_format= ''
+    elif data_type == "Integer":
+        is_inheritable = "NO" 
+        is_lookup = "NO"
+        show_at_entity_creation = "YES"
+        precision = ''
+        use_arbitrary_precision = ''
+        allowable_values= ''
+        lookup_table_name= ''
+        lookup_display_columns= '' 	
+        lookup_search_columns= ''
+        lookup_display_format= ''
+        lookup_sort_order= ''
+        export_format= ''
+
     # Create the new row data
     new_row = {
-        'Definition': f"GS1 Field_ID {field_id} {attribute_name}",
         'Attribute Type': 'Category',
+        'Attribute Name': f"CatSpec_{attribute_name}",
+        'Attribute Long Name': attribute_name_long,
         'Attribute Parent Name': 'Category Specific Attributes',
+        'Data Type' : data_type,
+        'Display Type' : display_type,
+        # 'Is Collection' : 't.b.d',
+        'Is Inheritable' : is_inheritable,
         'Is Localizable': 'NO',
         'Is Complex': 'NO',
+        'Is Lookup' : is_lookup,
         'Is Required': 'NO',
         'Is ReadOnly': 'NO',
         'Is Hidden': 'NO',
+        'Show At Entity Creation?': show_at_entity_creation,
         'Is Searchable': 'YES',
         'Is Null Value Search Required': 'YES',
         'Minimum Length': 0,
         'Maximum Length': 0,
+        'Precision' : precision,
+        'Use Arbitrary Precision?' : use_arbitrary_precision,
+        # 'UOM Type': 't.b.d.',
+        'Allowed UOMs': 't.d.b.',	
+        # 'Default UOM': 't.b.d.',
+        'Allowable Values':	allowable_values,
+        'LookUp Table Name': lookup_table_name,	
+        'Lookup Display Columns': lookup_display_columns,	
+        'Lookup Search Columns': lookup_search_columns,	
+        'Lookup Display Format': lookup_display_format,	
+        'Lookup Sort Order': lookup_sort_order,	
+        'Export Format': export_format,
+        # 'Sort Order' : 't.b.d',
+        'Definition': f"GS1 Field_ID {field_id} {description}",
         'Enable History': 'YES',
-        'Apply Time Zone Conversion': 'NO'
+        'Apply Time Zone Conversion': 'NO',
+        'Is UOM Localizable': 'NO'
     }
     
     # Add the new row to the list
